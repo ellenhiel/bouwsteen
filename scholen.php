a017e2bb2c07e9cc1d52368bf2dccd3d9c7a87c6
ellenhiel <elhi12@hotmail.com>
2021-12-29 16:48:58 +0100

    added tags

--- scholen.php
+++ scholen.php
@@ -53,9 +53,31 @@
     <link rel="stylesheet" href="index.css">
     <title>Bouwsteen</title>
     <link rel="icon" type="image/x-icon" href="/images/Final-logo_Bouwsteen.png">
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MQB3878');</script>
+<!-- End Google Tag Manager -->
+<!-- Hotjar Tracking Code for https://bouwsteen.site/ -->
+    <script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2763871,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MQB3878"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <nav>
         <a href="index.html"><img src="/images/Final-logo_Bouwsteen.png" alt="logo Bouwsteen"></a>
         <div class="fullnav">
